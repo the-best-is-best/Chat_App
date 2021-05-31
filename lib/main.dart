eeb6ca7343d9e429751402eb76646099a9eccf2f
HardiHood-Studio <johnraouf515@gmail.com>
2021-05-31 23:55:19 +0200

    Solve Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
-
-import 'package:chat_app/models/chat_model.dart';
-import 'package:chat_app/models/user_image_model.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+import '../models/chat_model.dart';
+import '../models/user_image_model.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import '../screens/chat_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -14,15 +12,7 @@ import 'models/auth_model.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    print('Got a message whilst in the foreground!');
-    print('Message data: ${message.data}');
-
-    if (message.notification != null) {
-      print('Message also contained a notification: ${message.notification}');
-    }
-  });
+  await Firebase.initializeApp();
 
   runApp(
     MultiProvider(
@@ -34,25 +24,6 @@ Future<void> main() async {
       child: MyApp(),
     ),
   );
-}
-
-// Crude counter to make messages unique
-int _messageCount = 0;
-
-/// The API endpoint here accepts a raw FCM payload for demonstration purposes.
-String constructFCMPayload(String token) {
-  _messageCount++;
-  return jsonEncode({
-    'token': token,
-    'data': {
-      'via': 'FlutterFire Cloud Messaging!!!',
-      'count': _messageCount.toString(),
-    },
-    'notification': {
-      'title': 'Hello FlutterFire!',
-      'body': 'This notification (#$_messageCount) was created via FCM!',
-    },
-  });
 }
 
 class MyApp extends StatelessWidget {
